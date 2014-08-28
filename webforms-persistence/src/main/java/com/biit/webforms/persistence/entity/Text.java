d2a9da9721001a2d38a1a7aa441a88c307d4f7b7
Jorge Izquierdo Ciges <jorizci@gmail.com>
2014-08-28 13:59:58 +0200

    Now text message content is displayed in Label column. If label is larger than 30, we get a shorter version.

--- webforms-persistence/src/main/java/com/biit/webforms/persistence/entity/Text.java
+++ webforms-persistence/src/main/java/com/biit/webforms/persistence/entity/Text.java
@@ -9,11 +9,13 @@ import javax.persistence.Table;
 
 import com.biit.form.BaseQuestion;
 import com.biit.form.TreeObject;
+import com.biit.form.exceptions.NotValidTreeObjectException;
 
 @Entity
 @Table(name = "texts")
 public class Text extends BaseQuestion {
 	public static final int MAX_DESCRIPTION_LENGTH = 10000;
+	public static final int MAX_DESCRIPTION_LABEL = 30;
 	
 	@Column(length = MAX_DESCRIPTION_LENGTH)
 	private String description;
@@ -29,9 +31,24 @@ public class Text extends BaseQuestion {
 	}
 
 	@Override
-	protected void copyData(TreeObject object) {
-		// TODO Auto-generated method stub
-
+	protected void copyData(TreeObject object) throws NotValidTreeObjectException {
+		if(object instanceof Text){
+			description = ((Text) object).getDescription();
+		}
+		throw new NotValidTreeObjectException("Copy data for Text only supports the same type copy");
+	}
+	
+	@Override
+	public String getLabel(){
+		if(description.length()>MAX_DESCRIPTION_LABEL){
+			return description.substring(0,MAX_DESCRIPTION_LABEL)+"...";
+		}
+		return description;
+	}
+	
+	@Override
+	public void setLabel(String label){
+		//Ignore
 	}
 
 	public String getDescription() {
