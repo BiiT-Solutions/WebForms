c2453e0e5c53e7bc46e7009aacdc6a34c5d2c676
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-12-17 17:59:08 +0100

    Imports subanswers from ABCD.

--- webforms-core/src/main/java/com/biit/webforms/utils/conversor/ConversorBaseAnswerToAnswer.java
+++ webforms-core/src/main/java/com/biit/webforms/utils/conversor/ConversorBaseAnswerToAnswer.java
@@ -1,12 +1,36 @@
 package com.biit.webforms.utils.conversor;
 
 import com.biit.form.BaseAnswer;
+import com.biit.form.TreeObject;
+import com.biit.form.exceptions.NotValidChildException;
+import com.biit.webforms.logger.WebformsLogger;
 import com.biit.webforms.persistence.entity.Answer;
 
-public class ConversorBaseAnswerToAnswer extends ConversorTreeObject<BaseAnswer, Answer>{
+public class ConversorBaseAnswerToAnswer extends ConversorTreeObject<BaseAnswer, Answer> {
 
 	@Override
 	public Answer createDestinyInstance() {
 		return new Answer();
+	}
+
+	@Override
+	public void copyData(BaseAnswer origin, Answer destiny) {
+		// Copy base data
+		super.copyData(origin, destiny);
+
+		// Convert and assign children
+		for (TreeObject child : origin.getChildren()) {
+			if (child instanceof BaseAnswer) {
+				BaseAnswer convertedChild = convert((BaseAnswer) child);
+				try {
+					destiny.addChild(convertedChild);
+				} catch (NotValidChildException e) {
+					// Impossible
+					WebformsLogger.errorMessage(this.getClass().getName(), e);
+				}
+			} else {
+				WebformsLogger.errorMessage(this.getClass().getName(), new Throwable("Child type not expected"));
+			}
+		}
 	}
 }
