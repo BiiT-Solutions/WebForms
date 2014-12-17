3eda38d197deca04e51f3b0139196f5613242385
Jorge Izquierdo Ciges <jorizci@gmail.com>
2014-12-17 12:05:59 +0100

    Added more functionality to the exporter.

--- webforms-core/src/main/java/com/biit/webforms/utils/exporters/xml/XmlExporter.java
+++ webforms-core/src/main/java/com/biit/webforms/utils/exporters/xml/XmlExporter.java
@@ -1,5 +1,6 @@
 package com.biit.webforms.utils.exporters.xml;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -19,6 +20,7 @@ public class XmlExporter {
 	private final Form form;
 	private final ComputedFlowView flows;
 	private HashMap<BaseQuestion, Integer> counters;
+	private HashMap<BaseQuestion, Integer> endFormCounters;
 
 	public XmlExporter(Form form) {
 		this.form = form;
@@ -29,8 +31,9 @@ public class XmlExporter {
 
 	private void initializeCounters() {
 		LinkedHashSet<TreeObject> questions = form.getAllChildrenInHierarchy(BaseQuestion.class);
-		for(TreeObject question: questions){
+		for (TreeObject question : questions) {
 			counters.put((BaseQuestion) question, new Integer(0));
+			endFormCounters.put((BaseQuestion) question, new Integer(0));
 		}
 	}
 
@@ -46,50 +49,75 @@ public class XmlExporter {
 		// Then at the next iteration to generate a new random path the next
 		// node to visit is always the node with the least visits. This solution
 		// prioritizes the appearance of each question.
-		
+
 		LinkedHashSet<TreeObject> questions = form.getAllChildrenInHierarchy(BaseQuestion.class);
-		if(questions.isEmpty()){
+		if (questions.isEmpty()) {
 			return;
 		}
-		
-		List<BaseQuestion> path = generatePath((BaseQuestion) questions.iterator().next(), questions.size());
-		
+
+		for (int i = 0; i < number; i++) {
+			List<BaseQuestion> path = generatePath((BaseQuestion) questions.iterator().next(), questions.size());
+			System.out.println(path);
+		}
 		
 	}
 
-	private List<BaseQuestion> generatePath(BaseQuestion startNode,int numberOfQuestion) throws ElementWithoutNextElement, TooMuchIterationsWhileGeneratingPath {
-		
+	private List<BaseQuestion> generatePath(BaseQuestion startNode, int numberOfQuestion)
+			throws ElementWithoutNextElement, TooMuchIterationsWhileGeneratingPath {
+
+		List<BaseQuestion> path = new ArrayList<BaseQuestion>();
+		path.add(startNode);
 		BaseQuestion currentNode = startNode;
 		int numberOfIterations = 0;
-		while(currentNode!=null){
-			if(numberOfIterations<numberOfQuestion){
+		while (currentNode != null) {
+			if (numberOfIterations < numberOfQuestion) {
 				throw new TooMuchIterationsWhileGeneratingPath();
 			}
 			numberOfIterations++;
-			Set<BaseQuestion> flows = getNextQuestions(currentNode);
+			Set<BaseQuestion> nextNode = getNextQuestions(currentNode);
+			path.add(getLeastUsedNexNode(currentNode, nextNode));
 		}
-		
-		return null;
+
+		return path;
 	}
-	
+
+	private BaseQuestion getLeastUsedNexNode(BaseQuestion currentNode, Set<BaseQuestion> nodes) {
+		BaseQuestion leastUsedNode = null;
+		int numberOfUsesOfLeastUsedNode = -1;
+
+		for (BaseQuestion node : nodes) {
+			int numberOfUses;
+			if (node != null) {
+				numberOfUses = endFormCounters.get(currentNode);
+			} else {
+				numberOfUses = counters.get(currentNode);
+			}
+			if (numberOfUses > numberOfUsesOfLeastUsedNode) {
+				numberOfUsesOfLeastUsedNode = numberOfUses;
+				leastUsedNode = currentNode;
+			}
+		}
+		return leastUsedNode;
+	}
+
 	private Set<Flow> getFlows(BaseQuestion currentNode) throws ElementWithoutNextElement {
-		if(flows.getFlowsByOrigin(currentNode).isEmpty()){
+		if (flows.getFlowsByOrigin(currentNode).isEmpty()) {
 			throw new ElementWithoutNextElement();
 		}
 		return flows.getFlowsByOrigin(currentNode);
 	}
-	
+
 	private Set<BaseQuestion> getNextQuestions(BaseQuestion currentNode) throws ElementWithoutNextElement {
 		Set<Flow> flows = getFlows(currentNode);
 		Set<BaseQuestion> nextQuestions = new HashSet<>();
-		for(Flow flow: flows){
+		for (Flow flow : flows) {
 			nextQuestions.add((BaseQuestion) flow.getDestiny());
 		}
 		return nextQuestions;
 	}
 
-	public void visitQuestion(BaseQuestion question){
-		counters.put(question,counters.get(question)+1);
+	public void visitQuestion(BaseQuestion question) {
+		counters.put(question, counters.get(question) + 1);
 	}
 
 }
