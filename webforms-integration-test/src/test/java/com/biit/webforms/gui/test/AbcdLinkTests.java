aae76e42afa6356e0bd1153fa9fe77d4cac2dae9
aperez <aperez@biit-solutions.com>
2015-02-18 17:47:05 +0100

    Created database properties reader for the gui tests

--- webforms-integration-test/src/test/java/com/biit/webforms/gui/test/AbcdLinkTests.java
+++ webforms-integration-test/src/test/java/com/biit/webforms/gui/test/AbcdLinkTests.java
@@ -2,36 +2,44 @@ package com.biit.webforms.gui.test;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
+import com.biit.utils.file.PropertiesFile;
+
 public class AbcdLinkTests extends WebFormsTester {
 
 	public static final String ABCD_SQL_DUMP_FILE_PATH = "./src/test/resources/AbcdFormDB.sql";
 	public static final String ABCD_SCRIPTRUNNER_OUTPUT_FILE_PATH = "./src/test/resources/AbcdFormDB.out";
-	public static final String ABCD_DB_URL = "jdbc:mysql://vagrant-mysql.biit-solutions.com:3306/abcdtest?useUnicode=true&amp;characterEncoding=UTF-8";
-	public static final String ABCD_DB_USER = "abcd";
-	public static final String ABCD_DB_PASS = "asd123";
 	private static final int TABLE_ROW = 0;
 	private static final int TREE_TABLE_ROW = 2;
 	public static final String ABCD_LINK_MESSAGE = "Form 'FormTest' version '1' has element 'Category2' and it's not found in form 'FormTest' version '1'.";
 	public static final String ABCD_DIFF_MESSAGE = "Form 'FormTest' version '1' has element 'Category1/Group' and it's not found in form 'FormTest' version '1'.";
+	private static final String DATABASE_CONFIG_FILE = "./target/classes/database.properties";
+	private static final String ABCD_DB_URL_PROPERTY = "abcd.hibernate.connection.url";
+	private static final String ABCD_DB_USER_PROPERTY = "abcd.hibernate.connection.username";
+	private static final String ABCD_DB_PASS_PROPERTY = "abcd.hibernate.connection.password";
 
 	@AfterClass
 	private void createAbcdForm() throws SQLException, ClassNotFoundException {
-		// Create MySql Connection
-		Connection con = DriverManager.getConnection(ABCD_DB_URL, ABCD_DB_USER, ABCD_DB_PASS);
 		try {
+			// Read the properties file
+			Properties prop = new Properties();
+			prop = PropertiesFile.load(DATABASE_CONFIG_FILE);
+			// Create MySql Connection
+			Connection con = DriverManager.getConnection(prop.getProperty(ABCD_DB_URL_PROPERTY),
+					prop.getProperty(ABCD_DB_USER_PROPERTY), prop.getProperty(ABCD_DB_PASS_PROPERTY));
 			// Initialize object for ScripRunner
 			ScriptRunner sr = new ScriptRunner(con);
 			// Give the input file to Reader
@@ -41,7 +49,7 @@ public class AbcdLinkTests extends WebFormsTester {
 			sr.runScript(reader);
 			sr.closeConnection();
 
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			System.err.println("Failed to Execute" + ABCD_SQL_DUMP_FILE_PATH + " The error is " + e.getMessage());
 			Assert.fail();
 		}
@@ -52,7 +60,7 @@ public class AbcdLinkTests extends WebFormsTester {
 		loginFormAdmin1();
 		getFormManagerPage().deleteAllCreatedForms();
 		logOut();
-		
+
 		loginFormEdit1();
 		// Import Abcd form
 		getFormManagerPage().clickNewButton();
