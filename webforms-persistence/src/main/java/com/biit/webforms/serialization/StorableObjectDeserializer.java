e4b32c22eddd7790350d05d496747ccb84f50b78
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-11-04 11:07:52 +0100

    Import image from json fixed.

--- webforms-persistence/src/main/java/com/biit/webforms/serialization/StorableObjectDeserializer.java
+++ webforms-persistence/src/main/java/com/biit/webforms/serialization/StorableObjectDeserializer.java
@@ -11,57 +11,56 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
-public class StorableObjectDeserializer<T extends StorableObject> implements JsonDeserializer<T>{
-	
-	public void deserialize(JsonElement json,JsonDeserializationContext context, T element){
+public class StorableObjectDeserializer<T extends StorableObject> implements JsonDeserializer<T> {
+
+	public void deserialize(JsonElement json, JsonDeserializationContext context, T element) {
 		JsonObject jobject = (JsonObject) json;
-		
-		element.setComparationId(parseString("comparationId",jobject,context));
+
+		element.setComparationId(parseString("comparationId", jobject, context));
 		element.setCreationTime(parseTimestamp("creationTime", jobject, context));
 		element.setUpdateTime(parseTimestamp("updateTime", jobject, context));
 		element.setCreatedBy(parseLong("createdBy", jobject, context));
 		element.setUpdatedBy(parseLong("updatedBy", jobject, context));
 	}
-	
-	public String parseString(String name,JsonObject jobject,JsonDeserializationContext context){
-		if(jobject.get(name)!=null){
+
+	public String parseString(String name, JsonObject jobject, JsonDeserializationContext context) {
+		if (jobject.get(name) != null) {
 			return (String) context.deserialize(jobject.get(name), String.class);
 		}
 		return null;
 	}
-	public Timestamp parseTimestamp(String name,JsonObject jobject,JsonDeserializationContext context){
-		if(jobject.get(name)!=null){
+
+	public Timestamp parseTimestamp(String name, JsonObject jobject, JsonDeserializationContext context) {
+		if (jobject.get(name) != null) {
 			return (Timestamp) context.deserialize(jobject.get(name), Timestamp.class);
 		}
 		return null;
 	}
-	
-	public Long parseLong(String name,JsonObject jobject,JsonDeserializationContext context){
-		if(jobject.get(name)!=null){
+
+	public Long parseLong(String name, JsonObject jobject, JsonDeserializationContext context) {
+		if (jobject.get(name) != null) {
 			return (Long) context.deserialize(jobject.get(name), Long.class);
 		}
 		return null;
 	}
-	
+
 	public Integer parseInteger(String name, JsonObject jobject, JsonDeserializationContext context) {
-		if(jobject.get(name)!=null){
+		if (jobject.get(name) != null) {
 			return (Integer) context.deserialize(jobject.get(name), Integer.class);
 		}
 		return null;
 	}
 
-	public boolean parseBoolean(String name, JsonObject jobject,
-			JsonDeserializationContext context) {
-		if(jobject.get(name)!=null){
+	public boolean parseBoolean(String name, JsonObject jobject, JsonDeserializationContext context) {
+		if (jobject.get(name) != null) {
 			return (Boolean) context.deserialize(jobject.get(name), Boolean.class);
 		}
 		return false;
 	}
-	
+
 	@Override
-	public T deserialize(JsonElement json, Type typeOfT,
-			JsonDeserializationContext context) throws JsonParseException {
-		
+	public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
 		final JsonObject jsonObject = json.getAsJsonObject();
 		Class<?> classType;
 		try {
@@ -70,7 +69,7 @@ public class StorableObjectDeserializer<T extends StorableObject> implements Jso
 		} catch (ClassNotFoundException e) {
 			WebformsLogger.errorMessage(this.getClass().getName(), e);
 		}
-		
+
 		return null;
 	}
 
--- webforms-persistence/src/main/java/com/biit/webforms/serialization/TreeObjectImageDeserializer.java
+++ webforms-persistence/src/main/java/com/biit/webforms/serialization/TreeObjectImageDeserializer.java
@@ -1,9 +1,12 @@
 package com.biit.webforms.serialization;
 
+import java.lang.reflect.Type;
+
 import com.biit.webforms.persistence.entity.TreeObjectImage;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 
 public class TreeObjectImageDeserializer extends StorableObjectDeserializer<TreeObjectImage> {
 
@@ -17,5 +20,18 @@ public class TreeObjectImageDeserializer extends StorableObjectDeserializer<Tree
 		element.fromBase64(parseString("data", jobject, context));
 
 		super.deserialize(json, context, element);
+	}
+
+	@Override
+	public TreeObjectImage deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		TreeObjectImage instance;
+		try {
+			instance = TreeObjectImage.class.newInstance();
+			deserialize(json, context, instance);
+			return instance;
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new JsonParseException(e);
+		}
 	}
 }
