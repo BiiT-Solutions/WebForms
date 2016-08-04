d7e19a852c13ac36c16f195e34505ba8867a122f
Jorge Hortelano <jhortelano@biit-solutions.com>
2016-08-04 16:22:23 +0200

    Fixed issue when accessing with a user from different application.

--- webforms-gui/src/main/java/com/biit/webforms/gui/webpages/NotFoundPage.java
+++ webforms-gui/src/main/java/com/biit/webforms/gui/webpages/NotFoundPage.java
@@ -3,12 +3,12 @@ package com.biit.webforms.gui.webpages;
 import com.biit.webforms.language.LanguageCodes;
 import com.biit.webforms.theme.ThemeIcons;
 
-public class NotFoundPage extends ErrorPage{
+public class NotFoundPage extends ErrorPage {
 	private static final long serialVersionUID = -5449790417580691336L;
 
 	public NotFoundPage() {
 		super();
-		
+
 		setLabelContent(LanguageCodes.PAGE_NOT_FOUND.translation());
 		setImageSource(ThemeIcons.PAGE_NOT_FOUND.getThemeResource());
 	}
