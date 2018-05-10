6f8795198973f40e04c08ec1e93ace453f9ddbde
Jorge Hortelano <jhortelano@biit-solutions.com>
2018-05-10 12:47:18 +0200

    Fixed NPE

--- webforms-persistence/src/main/java/com/biit/webforms/serialization/WebserviceCallInputLinkSerializer.java
+++ webforms-persistence/src/main/java/com/biit/webforms/serialization/WebserviceCallInputLinkSerializer.java
@@ -8,19 +8,20 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 
-public class WebserviceCallInputLinkSerializer extends StorableObjectSerializer<WebserviceCallInputLink>  {
+public class WebserviceCallInputLinkSerializer extends StorableObjectSerializer<WebserviceCallInputLink> {
 
 	@Override
-	public JsonElement serialize(WebserviceCallInputLink src, Type typeOfSrc,
-			JsonSerializationContext context) {
+	public JsonElement serialize(WebserviceCallInputLink src, Type typeOfSrc, JsonSerializationContext context) {
 		final JsonObject jsonObject = (JsonObject) super.serialize(src, typeOfSrc, context);
 
 		jsonObject.add("webservicePort", context.serialize(src.getWebservicePort()));
-		jsonObject.add("formElement_id", context.serialize(src.getFormElement().getPath()));
-		jsonObject.add("errors", context.serialize(src.getErrors()));		
+		if (src.getFormElement() != null) {
+			jsonObject.add("formElement_id", context.serialize(src.getFormElement().getPath()));
+		}
+		jsonObject.add("errors", context.serialize(src.getErrors()));
 		jsonObject.add("validationXpath", context.serialize(src.getValidationXpath()));
 
 		return jsonObject;
 	}
-	
+
 }
--- webforms-persistence/src/main/java/com/biit/webforms/serialization/WebserviceCallOutputLinkSerializer.java
+++ webforms-persistence/src/main/java/com/biit/webforms/serialization/WebserviceCallOutputLinkSerializer.java
@@ -8,18 +8,19 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 
-public class WebserviceCallOutputLinkSerializer extends StorableObjectSerializer<WebserviceCallOutputLink>  {
+public class WebserviceCallOutputLinkSerializer extends StorableObjectSerializer<WebserviceCallOutputLink> {
 
 	@Override
-	public JsonElement serialize(WebserviceCallOutputLink src, Type typeOfSrc,
-			JsonSerializationContext context) {
+	public JsonElement serialize(WebserviceCallOutputLink src, Type typeOfSrc, JsonSerializationContext context) {
 		final JsonObject jsonObject = (JsonObject) super.serialize(src, typeOfSrc, context);
-		
+
 		jsonObject.add("webservicePort", context.serialize(src.getWebservicePort()));
-		jsonObject.add("formElement_id", context.serialize(src.getFormElement().getPath()));
+		if (src.getFormElement() != null) {
+			jsonObject.add("formElement_id", context.serialize(src.getFormElement().getPath()));
+		}
 		jsonObject.add("isEditable", context.serialize(src.isEditable()));
 
 		return jsonObject;
 	}
-	
+
 }
