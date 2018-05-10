736520adea70bcaa140f23ef27c40326dac61a2b
Jorge Hortelano <jhortelano@biit-solutions.com>
2018-05-10 12:10:38 +0200

    Removed Hibernate Sequence table generation. Not needed any more

--- webforms-persistence/src/main/java/com/biit/webforms/persistence/WebformsSchemaExporter.java
+++ webforms-persistence/src/main/java/com/biit/webforms/persistence/WebformsSchemaExporter.java
@@ -99,7 +99,7 @@ public class WebformsSchemaExporter extends com.biit.persistence.JpaSchemaExport
 		gen.updateDatabaseScript(HibernateDialect.MYSQL, getDirectory(), getHost(), getPort(), getUser(), getPassword(), getDatabaseName());
 
 		// Add hibernate sequence table.
-		addTextToFile(createHibernateSequenceTable(), getDirectory() + File.separator + getOutputFile());
+		//addTextToFile(createHibernateSequenceTable(), getDirectory() + File.separator + getOutputFile());
 		// Add extra information from a external script.
 		addTextToFile(readFile(getScriptsToAdd(), Charset.forName("UTF-8")), getDirectory() + File.separator + getOutputFile());
 
--- webforms-release/database/create_mysql.sql
+++ webforms-release/database/create_mysql.sql
@@ -576,12 +576,3 @@
        foreign key (webservice_call) 
        references webservice_call (id);
 
-	CREATE TABLE `hibernate_sequence` (
-		`next_val` bigint(20) DEFAULT NULL
-	);
-
-	LOCK TABLES `hibernate_sequence` WRITE;
-	INSERT INTO `hibernate_sequence` VALUES (1);
-	UNLOCK TABLES;
-
-
