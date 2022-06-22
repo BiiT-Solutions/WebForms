1cab464015b7df4d7854332682e6cc17fb112de1
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-06-22 12:07:00 +0200

    Adding logs to webforms rest

--- webforms-core/src/main/java/com/biit/webforms/rest/FormRestService.java
+++ webforms-core/src/main/java/com/biit/webforms/rest/FormRestService.java
@@ -26,8 +26,9 @@ public class FormRestService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/json")
-    public Response getAppointmentExaminations(String petition) {
+    public Response getForm(String petition) {
         FormDescription parsedPetition;
+        WebformsLogger.info(FormRestService.class.getName(), "Requesting Form using endpoint '/forms/json' with payload '{}'.", petition);
         try {
             parsedPetition = parsePetition(petition);
             Form form = formDao.get(parsedPetition.formName, parsedPetition.getVersion(), parsedPetition.getOrganizationId());
@@ -35,7 +36,9 @@ public class FormRestService {
                 return Response.status(Response.Status.NOT_FOUND).entity("{\"error\":\"Unknown form with name '" + parsedPetition.getFormName() +
                         "' and version '" + parsedPetition.getVersion() + "' in organization '" + parsedPetition.getOrganizationId() + "'.\"}").build();
             }
-            return Response.ok(form.toJson(), MediaType.APPLICATION_JSON).build();
+            String json = form.toJson();
+            WebformsLogger.debug(FormRestService.class.getName(), "Form retrieved successfully:\n{} ", json);
+            return Response.ok(json, MediaType.APPLICATION_JSON).build();
         } catch (JsonSyntaxException ex) {
             WebformsLogger.errorMessage(this.getClass().getName(), ex);
             return Response.status(Response.Status.BAD_REQUEST).entity("{\"error\":\"Json syntax error\"}").build();
--- webforms-logger/src/main/java/com/biit/webforms/logger/WebformsLogger.java
+++ webforms-logger/src/main/java/com/biit/webforms/logger/WebformsLogger.java
@@ -12,63 +12,79 @@ import org.slf4j.LoggerFactory;
  */
 public class WebformsLogger extends BiitLogger {
 
-	private static Logger logger = LoggerFactory.getLogger(WebformsLogger.class);
+    private static Logger logger = LoggerFactory.getLogger(WebformsLogger.class);
 
-	/**
-	 * Events that have business meaning (i.e. creating category, deleting form,
-	 * ...). To follow user actions.
-	 */
-	public static void info(String className, String message) {
-		info(logger, className, message);
-	}
+    /**
+     * Events that have business meaning (i.e. creating category, deleting form,
+     * ...). To follow user actions.
+     */
+    public static void info(String className, String message) {
+        info(logger, className, message);
+    }
 
-	/**
-	 * Shows not critical errors. I.e. Email address not found, permissions not
-	 * allowed for this user, ...
-	 * 
-	 * @param message
-	 */
-	public static void warning(String className, String message) {
-		warning(logger, className, message);
-	}
+    public static void info(String className, String messageTemplate, Object... arguments) {
+        info(logger, className + ": " + messageTemplate, arguments);
+    }
 
-	/**
-	 * For following the trace of the execution. I.e. Knowing if the application
-	 * access to a method, opening database connection, etc.
-	 */
-	public static void debug(String className, String message) {
-		debug(logger, className, message);
-	}
+    /**
+     * Shows not critical errors. I.e. Email address not found, permissions not
+     * allowed for this user, ...
+     *
+     * @param message
+     */
+    public static void warning(String className, String message) {
+        warning(logger, className, message);
+    }
 
-	/**
-	 * To log any not expected error that can cause application malfuncionality.
-	 * 
-	 * @param message
-	 */
-	public static void severe(String className, String message) {
-		severe(logger, className, message);
-	}
+    public static void warning(String className, String messageTemplate, Object... arguments) {
+        warning(logger, className + ": " + messageTemplate, arguments);
+    }
 
-	/**
-	 * To log java exceptions and log also the stack trace. If enabled, also can
-	 * send an email to the administrator to alert of the error.
-	 * 
-	 * @param className
-	 * @param throwable
-	 */
-	public static void errorMessage(String className, Throwable throwable) {
-		String error = getStackTrace(throwable);
-		errorMessageNotification(logger, className, error);
-	}
+    /**
+     * For following the trace of the execution. I.e. Knowing if the application
+     * access to a method, opening database connection, etc.
+     */
+    public static void debug(String className, String message) {
+        debug(logger, className, message);
+    }
 
-	/**
-	 * Log a error message. If enabled, also can send an email to the
-	 * administrator to alert of the error.
-	 * 
-	 * @param className
-	 * @param throwable
-	 */
-	public static void errorMessage(String className, String message) {
-		errorMessageNotification(logger, className, message);
-	}
+    public static void debug(String className, String messageTemplate, Object... arguments) {
+        debug(logger, className + ": " + messageTemplate, arguments);
+    }
+
+    /**
+     * To log any not expected error that can cause application malfuncionality.
+     *
+     * @param message
+     */
+    public static void severe(String className, String message) {
+        severe(logger, className, message);
+    }
+
+    public static void severe(String className, String messageTemplate, Object... arguments) {
+        severe(logger, className + ": " + messageTemplate, arguments);
+    }
+
+    /**
+     * To log java exceptions and log also the stack trace. If enabled, also can
+     * send an email to the administrator to alert of the error.
+     *
+     * @param className
+     * @param throwable
+     */
+    public static void errorMessage(String className, Throwable throwable) {
+        String error = getStackTrace(throwable);
+        errorMessageNotification(logger, className, error);
+    }
+
+    /**
+     * Log a error message. If enabled, also can send an email to the
+     * administrator to alert of the error.
+     *
+     * @param className
+     * @param message
+     */
+    public static void errorMessage(String className, String message) {
+        errorMessageNotification(logger, className, message);
+    }
 }
