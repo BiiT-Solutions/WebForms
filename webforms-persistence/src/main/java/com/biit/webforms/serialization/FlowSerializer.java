898106b7ac63b3af1f259c2195976ae0078ad784
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-02-10 12:08:44 +0100

    Trying to skip invalid flows

--- webforms-persistence/src/main/java/com/biit/webforms/serialization/FlowSerializer.java
+++ webforms-persistence/src/main/java/com/biit/webforms/serialization/FlowSerializer.java
@@ -1,6 +1,7 @@
 package com.biit.webforms.serialization;
 
 import com.biit.form.json.serialization.StorableObjectSerializer;
+import com.biit.webforms.logger.WebformsLogger;
 import com.biit.webforms.persistence.entity.Flow;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -8,26 +9,34 @@ import com.google.gson.JsonSerializationContext;
 
 import java.lang.reflect.Type;
 
-public class FlowSerializer extends StorableObjectSerializer<Flow>{
+public class FlowSerializer extends StorableObjectSerializer<Flow> {
 
-	@Override
-	public JsonElement serialize(Flow src, Type typeOfSrc,
-			JsonSerializationContext context) {
-		final JsonObject jsonObject = (JsonObject) super.serialize(src, typeOfSrc, context);
-		
-		jsonObject.add("originId", context.serialize(src.getOrigin().getPath()));
-		jsonObject.add("flowType", context.serialize(src.getFlowType()));
-		if(src.getDestiny()!=null){
-			jsonObject.add("destinyId", context.serialize(src.getDestiny().getPath()));
-		}
-		jsonObject.add("others", context.serialize(src.isOthers()));
-		//If is a others flow, getCondition returns the negation of the sum of all other conditions.
-		//While the condition inner value is null.
-		if(!src.isOthers()){
-			jsonObject.add("condition", context.serialize(src.getComputedCondition()));
-		}
+    @Override
+    public JsonElement serialize(Flow src, Type typeOfSrc,
+                                 JsonSerializationContext context) {
+        final JsonObject jsonObject = (JsonObject) super.serialize(src, typeOfSrc, context);
 
-		return jsonObject;
-	}
-	
+        if (src.getOrigin() != null) {
+            jsonObject.add("originId", context.serialize(src.getOrigin().getPath()));
+        } else {
+            WebformsLogger.errorMessage(this.getClass().getName(), "Flow without originId!");
+        }
+        if (src.getOrigin() != null) {
+            jsonObject.add("flowType", context.serialize(src.getFlowType()));
+        } else {
+            WebformsLogger.errorMessage(this.getClass().getName(), "Flow without flowtype!");
+        }
+        if (src.getDestiny() != null) {
+            jsonObject.add("destinyId", context.serialize(src.getDestiny().getPath()));
+        }
+        jsonObject.add("others", context.serialize(src.isOthers()));
+        //If is an others flow, getCondition returns the negation of the sum of all other conditions.
+        //While the condition inner value is null.
+        if (!src.isOthers()) {
+            jsonObject.add("condition", context.serialize(src.getComputedCondition()));
+        }
+
+        return jsonObject;
+    }
+
 }
