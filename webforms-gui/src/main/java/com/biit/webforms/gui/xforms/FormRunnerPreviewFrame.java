640e2a134feb35578fd15da344f3d6c5506e3bd9
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-07-22 17:29:30 +0200

    Fixing NPE

--- webforms-gui/src/main/java/com/biit/webforms/gui/xforms/FormRunnerPreviewFrame.java
+++ webforms-gui/src/main/java/com/biit/webforms/gui/xforms/FormRunnerPreviewFrame.java
@@ -11,29 +11,29 @@ import com.vaadin.ui.UI;
  * Frame to open a preview of a FormRunner form in a new window.
  */
 public class FormRunnerPreviewFrame extends UI {
-	public final static String FORM_PARAMETER_TAG = "form";
-	public final static String FORM_VERSION_PARAMETER_TAG = "version";
-	public final static String FORM_ORGANIZATION_PARAMETER_TAG = "organization";
-	private static final long serialVersionUID = -4957704029911591631L;
+    public final static String FORM_PARAMETER_TAG = "form";
+    public final static String FORM_VERSION_PARAMETER_TAG = "version";
+    public final static String FORM_ORGANIZATION_PARAMETER_TAG = "organization";
+    private static final long serialVersionUID = -4957704029911591631L;
 
 
-	@Override
-	protected void init(VaadinRequest request) {
-		getPage().setTitle("Preview");
-		final String formName = request.getParameter(FORM_PARAMETER_TAG).trim();
-		final String formVersion = request.getParameter(FORM_VERSION_PARAMETER_TAG);
-		final String organization = request.getParameter(FORM_ORGANIZATION_PARAMETER_TAG);
+    @Override
+    protected void init(VaadinRequest request) {
+        getPage().setTitle("Preview");
+        final String formName = request.getParameter(FORM_PARAMETER_TAG) != null ? request.getParameter(FORM_PARAMETER_TAG).trim() : null;
+        final String formVersion = request.getParameter(FORM_VERSION_PARAMETER_TAG);
+        final String organization = request.getParameter(FORM_ORGANIZATION_PARAMETER_TAG);
         final String requestUrl = "preview?form=" + formName + "&version=" + formVersion + "&organization=" + organization;
 
-		String url = WebformsConfigurationReader.getInstance().getFormRunnerUrl();
+        String url = WebformsConfigurationReader.getInstance().getFormRunnerUrl();
 
-		WebformsLogger.debug(this.getClass().getName(), "Opening URL: " + url + "/" + requestUrl);
-		BrowserFrame browser = new BrowserFrame(null, new ExternalResource(url + "/" + requestUrl));
+        WebformsLogger.debug(this.getClass().getName(), "Opening URL: " + url + "/" + requestUrl);
+        BrowserFrame browser = new BrowserFrame(null, new ExternalResource(url + "/" + requestUrl));
 
-		browser.setImmediate(true);
-		browser.setSizeFull();
+        browser.setImmediate(true);
+        browser.setSizeFull();
 
-		setContent(browser);
-	}
+        setContent(browser);
+    }
 
 }
